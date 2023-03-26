11c0539e90f685e01d3c3f23a88088dd4f24321f
Skoldsjo <21369351+Skoldsjo@users.noreply.github.com>
2023-03-26 03:56:20 +0200

    Add files via upload

---
+++ public/scripts/hamburger.js
@@ -0,0 +1,16 @@
+const menuBtn = document.querySelector('.nav-icon');
+const menuList = document.querySelector('.small-nav');
+const navbar = document.getElementById('navbar');
+
+menuBtn.addEventListener('click', () => {
+    document.body.classList.toggle('is-visible')
+});
+
+menuLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+    })
+});
+
+
+
+
